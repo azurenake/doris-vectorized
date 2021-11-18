f972a3b890b50d879a419f9371dd9048cd387d59
cuikaifeng1 <cuikaifeng1@xiaomi.com>
2021-11-18 09:47:58 +0800

    Added licence.

--- be/src/vec/functions/nullif.cpp
+++ be/src/vec/functions/nullif.cpp
@@ -1,5 +1,19 @@
-#ifndef DORIS_FUNCTION_IFNULL_H
-#define DORIS_FUNCTION_IFNULL_H
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 #include "vec/functions/simple_function_factory.h"
 #include "vec/columns/column_nullable.h"
@@ -36,7 +50,7 @@ public:
         return arguments[0];
     }
 
-    NullPresence getNullPresense(const ColumnsWithTypeAndName& args) const{
+    NullPresence get_null_resense(const ColumnsWithTypeAndName& args) const {
         NullPresence res;
 
         for (const auto& elem : args) {
@@ -61,7 +75,7 @@ public:
         }
         
         if (!arguments.empty()) {
-            NullPresence null_presence = getNullPresense(arguments);
+            NullPresence null_presence = get_null_resense(arguments);
 
             if (null_presence.has_null_constant) {
                 return make_nullable(std::make_shared<DataTypeNothing>());
@@ -136,4 +150,3 @@ void register_function_nullif(SimpleFunctionFactory& factory) {
     factory.register_function<FunctionNullIf>();
 }
 }
-#endif //DORIS_FUNCTION_IFNULL_H
